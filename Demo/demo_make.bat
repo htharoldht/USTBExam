8c8d77e0b8c8d74fe48c7f15224534ae2f1afcf4
htharoldht <htharoldht@gmail.com>
2019-11-03 16:25:22 +0800

    修改批处理相关文件

--- Demo/demo_make.bat
+++ Demo/demo_make.bat
@@ -1,12 +1,12 @@
 @echo off
-xelatex 2017-2018-2_teacher.tex
-xelatex 2017-2018-2_teacher.tex
+xelatex demo_teacher.tex
+xelatex demo_teacher.tex
 
-xelatex 2017-2018-2_student.tex
-xelatex 2017-2018-2_student.tex
+xelatex demo_student.tex
+xelatex demo_student.tex
 
-xelatex 2017-2018-2_A3.tex
-xelatex 2017-2018-2_A3.tex
+xelatex demo_A3.tex
+xelatex demo_A3.tex
 
 del  *.aux *.bak *.bbl *.blg *.dbj *.dvi *.ent *.glg *.glo *.gls *.gz *.idx *.ilg *.ind *.lof *.log *.lot *.out *.ps *.thm *.synctex *.thm *.toc
 
