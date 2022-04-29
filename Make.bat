fec7fd2ffb20405aba9fd140ea090a93a5babb51
htharoldht <htharoldht@gmail.com>
2022-04-29 18:02:50 +0800

    添加latexindent格式化辅助文件

--- Make.bat
+++ Make.bat
@@ -1,4 +1,6 @@
 @echo off
+for %%B in (*.tex) do latexindent -w -m -l "%%B"
+
 xelatex USTBExam-teacher.tex
 xelatex USTBExam-teacher.tex
 
@@ -8,6 +10,6 @@ xelatex USTBExam-demo.tex
 xelatex USTBExam-A3.tex
 xelatex USTBExam-A3.tex
 
-del  *.aux *.bak *.bbl *.blg *.dbj *.dvi *.ent *.glg *.glo *.gls *.gz *.idx *.ilg *.ind *.lof *.log *.lot *.out *.ps *.thm *.synctex *.thm *.toc
+del  *.aux *.bak *.bbl *.blg *.dbj *.dvi *.ent *.glg *.glo *.gls *.gz *.idx *.ilg *.ind *.lof *.log *.lot *.out *.ps *.thm *.synctex *.thm *.toc *.bak*
 
 REM pause
