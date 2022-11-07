b27a6f142f7276ca81a58088c952ae9089a10f0e
htharoldht <htharoldht@gmail.com>
2022-11-07 13:53:31 +0800

    chore: synctex

--- Template/Make.bat
+++ Template/Make.bat
@@ -1,7 +1,7 @@
 @echo off
 for %%B in (*.tex) do latexindent -w -m -l "%%B"
 
-latexmk -xelatex -interaction=nonstopmode -shell-escape demo.tex
+latexmk -xelatex -interaction=nonstopmode -shell-escape -synctex=1 demo.tex
 
 del  *.aux, *.bbl, *.blg, *.idx, *.ind, *.lof, *.lot, *.out, *.toc, *.acn, *.acr, *.alg, *.glg, *.glo, *.gls, *.ist, *.fls, *.log, *.listing, *.hd, *.dvi, *.xdv, *.fdb_latexmk, *.bak*
 
