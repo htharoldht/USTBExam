4c0bc1807501d59e9aeada672df47bbff424489f
htharoldht <htharoldht@gmail.com>
2022-11-13 20:22:43 +0800

    feat: 格式化所有tex代码, 递归清除所有辅助文件

--- Make.bat
+++ Make.bat
@@ -1,8 +1,8 @@
 @echo off
-for %%B in (*.tex) do latexindent -w -m -l "%%B"
+for %%B in (**.tex) do latexindent -w -m -l "%%B"
 
 latexmk -xelatex -interaction=nonstopmode -shell-escape -synctex=1 demo.tex
 
-del  *.aux, *.bbl, *.blg, *.idx, *.ind, *.lof, *.lot, *.out, *.toc, *.acn, *.acr, *.alg, *.glg, *.glo, *.gls, *.ist, *.fls, *.log, *.listing, *.hd, *.dvi, *.xdv, *.fdb_latexmk, *.bak*
+del  **.aux, **.bbl, **.blg, **.idx, **.ind, **.lof, **.lot, **.out, **.toc, **.acn, **.acr, **.alg, **.glg, **.glo, **.gls, **.ist, **.fls, **.log, **.listing, **.hd, **.dvi, **.xdv, **.fdb_latexmk, **.bak*
 
 REM pause
